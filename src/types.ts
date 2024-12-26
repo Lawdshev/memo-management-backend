bbf84da7847ae84d33976fa4e8cdaa0dbce0499e
Sherif Okunade <o.d.alsherif@gmail.com>
2024-12-26 09:45:28 +0100

    fix:type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,5 @@
 import { Request } from 'express';
-import { Schema } from 'mongoose';
-import { IUser } from '../models/user';
+import { IUser } from './models/user';
 
 export type AuthedRequest = Request & {
     user: IUser
